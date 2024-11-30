dc9b975b2a0d248da528e7e2308ef24ab9ee0ddd
Gaurav ROy <40909110+gauravroy1995@users.noreply.github.com>
2024-11-30 13:06:48 +0530

    added fonts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        port: "",
+        pathname: "/my-bucket/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
